20c9839d34a18ae6fce7673c81a4565c8d3c8ab8
darshan45672 <drshnbhandary@gmail.com>
2024-06-27 09:45:48 +0530

    added cokumns and designed user page

--- ecom/app/Filament/Resources/UserResource.php
+++ ecom/app/Filament/Resources/UserResource.php
@@ -6,9 +6,14 @@ use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Resources\Pages\CreateRecord;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +28,10 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')->label('Name')->required()->placeholder('Darshan Bhandary'),
+                TextInput::make('email')->label('Email')->required()->placeholder('example@example.com')->unique(ignoreRecord: true),
+                DateTimePicker::make('email_verified_at')->label('Email Verified At')->default(now()),
+                TextInput::make('password')->label('Password')->required(fn(Page $livewire): bool => $livewire instanceof CreateRecord)->password()->placeholder('********')->dehydrated(fn($state) => filled($state)),
             ]);
     }
 
@@ -31,13 +39,20 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->label('Name')->searchable()->sortable(),
+                TextColumn::make('email')->label('Email')->searchable()->sortable(),
+                TextColumn::make('email_verified_at')->label('Email Verified At')->searchable()->sortable()->dateTime(),
+                TextColumn::make('created_at')->label('Created At')->searchable()->sortable()->dateTime(),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
