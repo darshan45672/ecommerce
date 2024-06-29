d4b5bc8bc90facbd2b985e04d028252f4581b7ca
darshan45672 <drshnbhandary@gmail.com>
2024-06-29 08:02:46 +0530

    added product crud detials to admin

--- ecom/app/Filament/Resources/ProductResource.php
+++ ecom/app/Filament/Resources/ProductResource.php
@@ -6,6 +6,12 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -17,14 +23,34 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
-            ]);
+                Group::make([
+                    Section::make('Product Information')->schema([
+                        TextInput::make('name')->required()->label('Product Name')->maxLength(255),
+                        TextInput::make('slug')->required()->maxLength(255)->disabled(),
+                        MarkdownEditor::make('description')->required()->label('Product Description')->columnSpanFull()->fileAttachmentsDirectory('products'),
+                    ])->columns(2),
+
+                    Section::make('Images')->schema([
+                        FileUpload::make('images')->label('Product Images')->image()->multiple()->directory('products')->maxFiles(5)->reorderable(),
+                    ])
+                ])->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('Price')->schema([
+                        TextInput::make('price')->required()->label('Product Price')->prefix('₹')->numeric(),
+                    ]),
+                    Section::make('Associations')->schema([
+                        Select::make('category_id')->label('Category')->required()->searchable()->preload()->relationship('category', 'name'),
+                        Select::make('brand_id')->label('Brand')->required()->searchable()->preload()->relationship('brand', 'name'),
+                    ])
+                ])->columnSpan(1),
+            ])->columns(3);
     }
 
     public static function table(Table $table): Table
