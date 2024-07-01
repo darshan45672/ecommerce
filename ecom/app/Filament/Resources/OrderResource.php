790d41f7f2b331c214cf18a1b213c6fe6696e566
darshan45672 <drshnbhandary@gmail.com>
2024-07-01 07:46:50 +0530

    added relationship manager to order page

--- ecom/app/Filament/Resources/OrderResource.php
+++ ecom/app/Filament/Resources/OrderResource.php
@@ -174,7 +174,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            AddressRelationManager::class,
+            // AddressRelationManager::class,
         ];
     }
 
